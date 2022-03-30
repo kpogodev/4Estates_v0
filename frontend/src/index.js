6c79a50c8ce79ccd8cb8ef5af444bc9fa7a63b85
kpogodev <k.pogorzelski.dev@gmail.com>
2022-03-31 00:17:52 +0100

    Front: Updating React 17 --> 18, Trying Suspense Component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import React from 'react'
+import * as ReactDOMClient from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root')
+
+const root = ReactDOMClient.createRoot(container)
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
--- frontend/src/pages/ManageProperty.jsx
+++ frontend/src/pages/ManageProperty.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, Suspense } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useParams, Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
@@ -33,34 +33,38 @@ function ManageProperty() {
     }
   }, [isSuccess, isError, message, dispatch])
 
-  if (!property) return <Loading />
+  // if (!property) return <Loading />
 
   return (
-    <motion.div
-      variants={pageTransition}
-      initial='hidden'
-      animate='visible'
-      exit='exit'
-      className='w-full grid grid-cols-12 gap-10'
-    >
-      <div className='col-span-12 flex justify-between items-center gap-5 flex-wrap'>
-        <h2 className='block text-left font-bold text-2xl xl:text-4xl xl:py-4'>
-          Manage Property
-          <span className='!block text-lg text-gray-500 font-medium'>{property.location.formatted_address}</span>
-        </h2>
-        <Link to='/dashboard' className='btn btn-link px-0'>
-          Back to Dashboard
-        </Link>
-      </div>
-      <div className='flex flex-col col-span-8 gap-10'>
-        <PropertySlider className='max-h-[600px] min-h-[430px] xl:min-h-[600px] bg-black shadow-lg' />
-        <PropertySliderUpload className='row-span-1' />
-        <PropertyLocation />
-      </div>
-      <div className='col-span-4 flex flex-col gap-10'>
-        <PropertyDetails className='row-span-2' />
-      </div>
-    </motion.div>
+    property && (
+      <Suspense fallback={<Loading />}>
+        <motion.div
+          variants={pageTransition}
+          initial='hidden'
+          animate='visible'
+          exit='exit'
+          className='w-full grid grid-cols-12 gap-10'
+        >
+          <div className='col-span-12 flex justify-between items-center gap-5 flex-wrap'>
+            <h2 className='block text-left font-bold text-2xl xl:text-4xl xl:py-4'>
+              Manage Property
+              <span className='!block text-lg text-gray-500 font-medium'>{property.location.formatted_address}</span>
+            </h2>
+            <Link to='/dashboard' className='btn btn-link px-0'>
+              Back to Dashboard
+            </Link>
+          </div>
+          <div className='flex flex-col col-span-8 gap-10'>
+            <PropertySlider className='max-h-[600px] min-h-[430px] xl:min-h-[600px] bg-black shadow-lg' />
+            <PropertySliderUpload className='row-span-1' />
+            <PropertyLocation />
+          </div>
+          <div className='col-span-4 flex flex-col gap-10'>
+            <PropertyDetails className='row-span-2' />
+          </div>
+        </motion.div>
+      </Suspense>
+    )
   )
 }
 
